caa324bd1ce9db0ce8a09f23d31abb9317326377
Guo Mingyu <v-mingu@microsoft.com>
2013-04-06 20:04:41 +0800

    init graph project.

---
+++ graph/src/graph.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : graph.cpp
+// Author      : Guo Mingyu
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl; // prints Hello World
+	return 0;
+}
